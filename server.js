0b7c5f413a6f7a88df62a1e0324a39ccc6b32c06
vickey1213 <vickey.kmr90@gmail.com>
2023-02-13 20:11:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:pass_123@devconnector.agdmw.mongodb.net/black-cofferr?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  });
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  console.log("header host = ", req.headers.host);
+  res.redirect("https://google.com");
+});
+
+const apiv1_routes = require("./routes");
+
+app.use("/v1/", apiv1_routes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`listening on the port ${port}`));
